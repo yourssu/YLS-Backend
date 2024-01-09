812516332876ebb7ccfd062222f721c57043760f
shon5544 <sbs8239@gmail.com>
2024-01-09 23:32:42 +0900

    add: initial commit

---
+++ src/main/kotlin/com/yourssu/yls/YlsApplication.kt
@@ -0,0 +1,11 @@
+package com.yourssu.yls
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YlsApplication
+
+fun main(args: Array<String>) {
+	runApplication<YlsApplication>(*args)
+}
---
+++ src/test/kotlin/com/yourssu/yls/YlsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yourssu.yls
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YlsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
