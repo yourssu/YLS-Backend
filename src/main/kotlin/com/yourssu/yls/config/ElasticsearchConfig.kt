538cf195dde3ed4382b68b299e9e9d0c3de390f1
shon5544 <sbs8239@gmail.com>
2024-01-15 17:28:51 +0900

    feat: ES 관련 config 추가

---
+++ src/main/kotlin/com/yourssu/yls/config/ElasticsearchConfig.kt
@@ -0,0 +1,15 @@
+package com.yourssu.yls.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.elasticsearch.client.ClientConfiguration
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration
+
+@Configuration
+class ElasticsearchConfig: ElasticsearchConfiguration() {
+    override fun clientConfiguration(): ClientConfiguration {
+        return ClientConfiguration
+            .builder()
+            .connectedTo("localhost:9200")
+            .build()
+    }
+}
