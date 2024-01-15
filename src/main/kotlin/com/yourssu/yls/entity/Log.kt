5d43384336951508b465db2474c143d6f623cb7b
shon5544 <sbs8239@gmail.com>
2024-01-15 17:29:53 +0900

    fix: event 필드 Nested 타입으로 변경

--- src/main/kotlin/com/yourssu/yls/entity/Log.kt
+++ src/main/kotlin/com/yourssu/yls/entity/Log.kt
@@ -2,10 +2,13 @@ package com.yourssu.yls.entity
 
 import com.yourssu.yls.common.EventVO
 import org.springframework.data.elasticsearch.annotations.Document
+import org.springframework.data.elasticsearch.annotations.Field
+import org.springframework.data.elasticsearch.annotations.FieldType
 
 @Document(indexName = "logs")
 class Log(
-    val user: String,
-    val timestamp: String,
-    val event: EventVO
+    private val user: String,
+    private val timestamp: String,
+    @Field(type = FieldType.Nested)
+    private val event: EventVO,
 )
