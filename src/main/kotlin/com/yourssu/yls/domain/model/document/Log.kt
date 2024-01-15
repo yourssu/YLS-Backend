fabef09321195c18a68691001473ba4d892f09e5
shon5544 <sbs8239@gmail.com>
2024-01-15 17:48:27 +0900

    fix: timestamp LocalDateTime으로 수정

--- src/main/kotlin/com/yourssu/yls/domain/model/document/Log.kt
+++ src/main/kotlin/com/yourssu/yls/domain/model/document/Log.kt
@@ -4,11 +4,12 @@ import com.yourssu.yls.domain.model.nested.EventVO
 import org.springframework.data.elasticsearch.annotations.Document
 import org.springframework.data.elasticsearch.annotations.Field
 import org.springframework.data.elasticsearch.annotations.FieldType
+import java.time.LocalDateTime
 
 @Document(indexName = "logs")
 class Log(
     private val user: String,
-    private val timestamp: String,
+    private val timestamp: LocalDateTime,
     @Field(type = FieldType.Nested)
     private val event: EventVO,
 )
