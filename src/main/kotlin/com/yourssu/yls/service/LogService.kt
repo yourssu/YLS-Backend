81e6d7b7bace743fc69e9bbb3b081c33b0feb1f3
eckrin <bsh51515@naver.com>
2024-01-16 22:44:31 +0900

    feat: Document에 Random Id값 부여

--- src/main/kotlin/com/yourssu/yls/service/LogService.kt
+++ src/main/kotlin/com/yourssu/yls/service/LogService.kt
@@ -7,6 +7,7 @@ import com.yourssu.yls.repository.LogRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.security.SecureRandom
 
 @Service
 class LogService (
@@ -20,7 +21,11 @@ class LogService (
 
     @Transactional
     fun saveLog(log: Log): LogDto {
-        val saveLog = logRepository.save(log)
+        var randomId: Long
+        do {
+            randomId = SecureRandom().nextLong()
+        } while(randomId<=0)
+        val saveLog = logRepository.save(Log(randomId, log.user, log.timestamp, log.event))
         return LogDto(saveLog.id, saveLog.user, saveLog.timestamp, saveLog.event)
     }
 }
