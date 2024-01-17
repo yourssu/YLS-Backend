3ca82d44648f7622b34364952bbec93db8800685
eckrin <bsh51515@naver.com>
2024-01-18 00:46:06 +0900

    fix: 클라이언트측 timestamp 사용하도록 수정

--- src/main/kotlin/com/yourssu/yls/domain/service/LogAssembler.kt
+++ src/main/kotlin/com/yourssu/yls/domain/service/LogAssembler.kt
@@ -1,49 +1,45 @@
 package com.yourssu.yls.domain.service
 
-import com.yourssu.yls.app.request.LoggingRequest
-import com.yourssu.yls.app.response.EventResponse
-import com.yourssu.yls.app.response.LoggingResponse
+import com.yourssu.yls.application.request.LoggingRequest
+import com.yourssu.yls.application.response.EventResponse
+import com.yourssu.yls.application.response.LoggingResponse
 import com.yourssu.yls.domain.model.document.LogDocument
 import com.yourssu.yls.domain.model.nested.EventVO
-import java.time.LocalDateTime
 
-class LogAssembler {
-    companion object {
-        fun writeDto(logDocument: LogDocument): LoggingResponse {
-            val eventDocument = logDocument.event
-            val eventRequest =
-                EventResponse(
-                    platform = eventDocument.platform,
-                    screen = eventDocument.screen,
-                    name = eventDocument.name,
-                    keyword = eventDocument.keyword,
-                    resultID = eventDocument.resultID,
-                )
-
-            return LoggingResponse(
-                user = logDocument.user,
-                timestamp = logDocument.timestamp.toString(),
-                event = eventRequest,
+object LogAssembler {
+    fun writeDto(logDocument: LogDocument): LoggingResponse {
+        val eventDocument = logDocument.event
+        val eventRequest =
+            EventResponse(
+                platform = eventDocument.platform,
+                screen = eventDocument.screen,
+                name = eventDocument.name,
+                keyword = eventDocument.keyword,
+                resultID = eventDocument.resultID,
             )
-        }
 
-        fun writeDocument(loggingRequest: LoggingRequest): LogDocument {
-            val requestEvent = loggingRequest.event
-            val eventVO =
-                EventVO(
-                    platform = requestEvent.platform,
-                    screen = requestEvent.screen,
-                    name = requestEvent.name,
-                    keyword = requestEvent.keyword,
-                    resultID = requestEvent.resultID,
-                )
+        return LoggingResponse(
+            user = logDocument.user,
+            timestamp = logDocument.timestamp,
+            event = eventRequest,
+        )
+    }
 
-            return LogDocument(
-                user = loggingRequest.user,
-                // 수정
-                timestamp = LocalDateTime.now().toString(),
-                event = eventVO,
+    fun writeDocument(loggingRequest: LoggingRequest): LogDocument {
+        val requestEvent = loggingRequest.event
+        val eventVO =
+            EventVO(
+                platform = requestEvent.platform,
+                screen = requestEvent.screen,
+                name = requestEvent.name,
+                keyword = requestEvent.keyword,
+                resultID = requestEvent.resultID,
             )
-        }
+
+        return LogDocument(
+            user = loggingRequest.user,
+            timestamp = loggingRequest.timestamp,
+            event = eventVO,
+        )
     }
 }
