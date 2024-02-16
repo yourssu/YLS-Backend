a12078bf19915ad8cb5cb606dbaad3ad57d204e0
shon5544 <sbs8239@gmail.com>
2024-02-16 21:32:15 +0900

    fix: ElasticsearchConfig 설정 수정

--- src/main/kotlin/com/yourssu/yls/global/config/ElasticsearchConfig.kt
+++ src/main/kotlin/com/yourssu/yls/global/config/ElasticsearchConfig.kt
@@ -1,5 +1,6 @@
 package com.yourssu.yls.global.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.elasticsearch.client.ClientConfiguration
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration
@@ -7,11 +8,19 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 
 @Configuration
 @EnableElasticsearchRepositories
-class ElasticsearchConfig : ElasticsearchConfiguration() {
+class ElasticsearchConfig(
+    @Value("\${es.id}")
+    val user: String,
+    @Value("\${es.password}")
+    val password: String,
+    @Value("\${es.endpoint}")
+    val endpoint: String,
+) : ElasticsearchConfiguration() {
     override fun clientConfiguration(): ClientConfiguration {
         return ClientConfiguration
             .builder()
-            .connectedTo("localhost:9200")
+            .connectedTo(endpoint)
+            .withBasicAuth(user, password)
             .build()
     }
 }
